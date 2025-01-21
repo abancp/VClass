46e33a4587a070c4d065cb99e629b71e90a582f8
Aban Muhammed C P <abancpchengani@gmail>
2025-01-21 21:28:15 +0530

    server setup for deplay

--- client/src/config/SERVER_URL.js
+++ client/src/config/SERVER_URL.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "https://vclass.up.railway.app"
-//export const SERVER_URL = "http://localhost:5000"
+//export const SERVER_URL = "https://vclass.up.railway.app"
+export const SERVER_URL = "http://localhost:5000"
