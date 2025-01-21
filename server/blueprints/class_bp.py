5b3c501057f30f3f3c60eb386daaa19ba05ba53a
Aban Muhammed C P <abancpchengani@gmail>
2025-01-21 20:15:02 +0530

    server setup for deplay

--- server/blueprints/class_bp.py
+++ server/blueprints/class_bp.py
@@ -34,7 +34,7 @@ def create_class(userdata):
 
 @class_bp.route("/<class_id>",methods=['GET'])
 @member_require
-def get_class(class_id):
+def get_class(class_id,userdata):
     try:
         class_ = classes.find_one({"_id":ObjectId(class_id)},{'_id':1,'name':1,'subject':1,'description':1,'key':1,'number_of_students':1,'creater':1})
         class_['_id'] = str(class_['_id'])
@@ -45,7 +45,7 @@ def get_class(class_id):
 
 @class_bp.route("/peoples/<class_id>",methods=['GET'])
 @member_require
-def get_peoples(class_id):
+def get_peoples(class_id,userdata):
     pipeline = [
     {"$match": {"_id": ObjectId(class_id)}},
     {
