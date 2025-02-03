57f194f0ef70fcaa33b346c657870fb9b91523b0
Aban Muhammed C P <abancpchengani@gmail>
2025-02-03 20:59:17 +0530

    baackend url changed

--- client/src/config/SERVER_URL.js
+++ client/src/config/SERVER_URL.js
@@ -1,2 +1,2 @@
 //export const SERVER_URL = "https://vclass.up.railway.app"
-export const SERVER_URL = process.env.NODE_ENV === "development" ?"http://localhost:5000":"https://vclass.onrender.com"
+export const SERVER_URL = process.env.NODE_ENV === "development" ?"http://localhost:5000":"https://vclass-production.up.railway.app"
