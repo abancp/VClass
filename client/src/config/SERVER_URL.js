6556370fd52d1d10a52e06ae37763b1d8785d369
Aban Muhammed C P <132763024+abancp@users.noreply.github.com>
2025-02-05 10:49:09 +0530

    Update SERVER_URL.js

--- client/src/config/SERVER_URL.js
+++ client/src/config/SERVER_URL.js
@@ -1,3 +1,3 @@
 //export const SERVER_URL = "https://vclass.up.railway.app"
-export const SERVER_URL = process.env.NODE_ENV === "development" ?"http://localhost:5000":"https://abancp.pythonanywhere.com"
+export const SERVER_URL = process.env.NODE_ENV === "development" ?"http://localhost:5000":"https://vclass.onrender.com"
 //https://vclass-production.up.railway.app
