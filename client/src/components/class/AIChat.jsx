05a595d4584680baf4af8bb6d765b04dc6e9debe
Aban Muhammed C P <abancpchengani@gmail>
2025-02-06 22:11:32 +0530

    ai page updated

--- client/src/components/class/AIChat.jsx
+++ client/src/components/class/AIChat.jsx
@@ -4,17 +4,17 @@ import axios from 'axios'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from "remark-gfm"
 
-function AIChat({id,chats,setChats}) {
+function AIChat({ id, chats, setChats }) {
   const [thinking, setThinking] = useState(false)
   const [prompt, setPrompt] = useState("")
 
   const handleGenerate = (e) => {
-    e.preventDefault()
+    e && e.preventDefault()
     if (thinking) return
     setPrompt("")
     setThinking(true)
     setChats((prev) => [...prev, { user: prompt }])
-    axios.post(SERVER_URL + "/ai/"+id+"/gen", { prompt: prompt }, { withCredentials: true })
+    axios.post(SERVER_URL + "/ai/" + id + "/gen", { prompt: prompt }, { withCredentials: true })
       .then(({ data }) => {
         if (data.success) {
           setThinking(false)
@@ -32,7 +32,7 @@ function AIChat({id,chats,setChats}) {
   return (
     <div className='w-full flex flex-col gap-2 items-center justify-end  p-3 h-[calc(100vh-3.5rem)] '>
       <div className='gap-3 items-center flex flex-col overflow-y-scroll h-[calc(100vh-10rem)] w-[50rem] p-3 w-full'>
-        {
+        {chats.length !== 0 ?
           chats.map((chat) => {
             let isUser = chat.user !== undefined;
             return (
@@ -43,26 +43,51 @@ function AIChat({id,chats,setChats}) {
                 </div>
               </div>
             );
-          })
+          }) :
+          <div className='w-full h-full justify-center items-center gap-3 flex flex-col'>
+            <h1 className='text-4xl font-bold'>How Can i Help You ?</h1>
+            <div className='cursor-pointer  duration-300 p-2 rounded-2xl w-[50rem] flex-col flex bg-secondery items-center justify-center'>
+              <form className='w-full flex h-full' onSubmit={handleGenerate}>
+                <textarea placeholder='you can ask anything about this class , works and other complex prompts' onChange={(e) => setPrompt(e.target.value)} value={prompt} onKeyDown={handleEnter} name="input_area" className='w-full focus:outline-none bg-transparent min-h-[5rem] max-h-[10rem] '></textarea>
+                <div className='flex w-fit px-3 items-end justify-end'>
+                  <button type='submit'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor" className="bi bi-arrow-up-circle-fill text-light" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+                    </svg>
+                  </button>
+                </div>
+              </form>
+
+            </div>
+            <div className='w-full flex justify-center gap-3'>
+              <div onClick={()=>{setPrompt("How Many Assignments in this class");handleGenerate()}} className='p-2 px-3 cursor-pointer rounded-2xl border border-tersiory/80'>How Many </div>
+              <div className='p-2 px-3 cursor-pointer rounded-2xl border border-tersiory/80'>List the Students submitted Assignment-1</div>
+              <div className='p-2 px-3 cursor-pointer rounded-2xl border border-tersiory/80'>List Teachers</div>
+            </div>
+          </div>
         }
         {thinking &&
           <div className='flex w-[50rem] justify-start'>
             <div className='p-2'>...</div>
           </div>
         }
-      </div>
-      <div className='cursor-pointer  duration-300 p-2 rounded-md w-[50rem] flex-col flex bg-secondery items-center justify-center'>
-        <form className='w-full flex h-full' onSubmit={handleGenerate}>
-          <textarea onChange={(e) => setPrompt(e.target.value)} value={prompt} onKeyDown={handleEnter} name="input_area" className='w-full focus:outline-none rounded-md bg-transparent min-h-[2rem] max-h-[10rem] h-full'></textarea>
-          <div className='flex w-fit px-3 items-end justify-end'>
-            <button type='submit'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-up-circle-fill text-light" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
-              </svg>
-            </button>
-          </div>
-        </form>
-      </div>
+      </div>{
+        chats.length !== 0 &&
+        <div className=' duration-300 p-2 rounded-md w-[50rem] flex-col flex bg-secondery items-center justify-center'>
+          <form className='w-full flex h-full' onSubmit={handleGenerate}>
+            <textarea placeholder='you can ask anything about this class , works and other complex prompts' onChange={(e) => setPrompt(e.target.value)} value={prompt} onKeyDown={handleEnter} name="input_area" className='w-full focus:outline-none rounded-md bg-transparent min-h-[3rem] max-h-[10rem] h-full'></textarea>
+            <div className='flex w-fit px-3 items-end justify-end'>
+              <button type='submit'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-up-circle-fill text-light" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+                </svg>
+              </button>
+            </div>
+          </form>
+        </div>
+
+
+      }
       <p className='font-extralight text-xs opacity-60'>VClass AI can make mistakes </p>
     </div >
   )
--- server/blueprints/ai.py
+++ server/blueprints/ai.py
@@ -19,7 +19,12 @@ def gen(class_id,userdata):
         system_prompt = "you are a ai assinstand for a virtual class named VClass . users asking questions by prompt. generate positive response depends on users role,name,and prompt : "
         general_prompt = system_prompt+ f"[ role :{userdata['role']} ,username:{userdata['username']}, prompt:{data['prompt']} ]"
         mongo_system_prompt = """you are a query generator for a virtual class named VClass Application . schema of mongodb { "classes": { "_id": "ObjectId", "name": "String", "subject": "String", "description": "String", "students": ["ObjectId"], "teachers": ["ObjectId"], "key": "String", "number_of_students": "Number", "creater": "ObjectId" }, "works": { "_id": "ObjectId", "class_id": "ObjectId", "title": "String", "instruction": "String", "students": ["String"], "due_date": "Long", "can_edit": "Boolean", "teacher": "ObjectId", "teacher_name": "String", "type": "String", "accept_submits": "Boolean", "time": "Long" }, "submits": { "_id": "ObjectId", "work_id": "ObjectId", "class_id": "ObjectId", "user_id": "ObjectId", "data": "Object", "time": "Long" }, "users": { "_id": "ObjectId", "username": "String", "email": "String", "password": "Binary", "student": ["ObjectId"], "teacher": ["ObjectId"] } } . the collections are classes,users,works,submits . you need convert user promt to mongodb query consider above schema.you need generate only mongodb query based on user prompt .
+
+if prompt is need access other than this class's scope . generate <no_permission>
+
 generate query like db.collection.aggregate(...).
+
+
 consider 1:  the class id will be given in the time execution of this query . you are in a class room . a classroom have a mongodb _id . use <class_id> when generating query we will replace that with actual class_id
 imoprtant : all json object keys mut be enclosed in double quates
 the user only have permission to perform any operation in out of the class . if anything smells wrong . generate <no_permission>
@@ -50,7 +55,9 @@ user prompt :
 
         response = model.generate_content(mongo_system_prompt+data['prompt'])
         print(response.text)
-        if "<general>" in response.text:
+        if "<no_permission>" in response.text:
+            return jsonify({"success":True,"response":"for security . we can 't proceed with your question"})
+        elif "<general>" in response.text:
             response = model.generate_content(general_prompt)
             return jsonify({"success":True,"response":response.text})
         else:
@@ -89,7 +96,7 @@ user prompt :
                 result = list(db[collection_name].aggregate(pipeline)) 
             else:
                 result = "something went wrong with database operation"
-            response = model.generate_content(f"""you are a ai assinstand in virtual class named VClass user askeda question and answer given here generate a response for the prompt from user with reuslt from database .nombers are express in numeric way not in english . if user ask for a date or time the database will return date or time as unixbased milliseconds you need convert it . make response with natural language regarding username {userdata['username']} is a {userdata['role']} in class prompt : {data['prompt']} result : {str(result)} """)
+            response = model.generate_content(f"""you are a ai assinstand in virtual class named VClass user askeda question and answer given here generate a response for the prompt from user with reuslt from database .nombers are express in numeric way not in english . if user ask for a date or time the database will return date or time as unixbased milliseconds you need convert it . make response with natural language . userdata :  [username {userdata['username']} is a {userdata['role']} ] , prompt : {data['prompt']} , result : {str(result)} """)
 
             print(result)
             return jsonify({"success":True,"response":response.text})  
